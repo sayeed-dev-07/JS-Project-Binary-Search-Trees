2dcdd88bc18293427f114a6aa6a00752e0f36258
sayeed <expsayeedshorif@gmail.com>
2025-05-02 21:52:43 +0600

    set up the blank sheet will code tommorow

---
+++ node.js
@@ -0,0 +1,8 @@
+export default class Node{
+    constructor(data, left = null, right = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
---
+++ tree.js
@@ -0,0 +1,15 @@
+import Node from "./node.js";
+
+export default class Tree{
+    constructor(arr){
+        this.arr = arr;
+        this.root = null;
+    }
+    buildTree(arr){
+        this.#sortArray(arr)
+    }
+    #sortArray(arr){
+        let newSortedArr = [];
+
+    }
+}
